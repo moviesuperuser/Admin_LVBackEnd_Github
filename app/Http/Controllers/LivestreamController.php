8752813834380992f6956fd2d6bc28a54a149419
annguyenbku1999 <78542632+annguyenbku1999@users.noreply.github.com>
2021-05-25 23:30:09 +0700

    Update LivestreamController.php

--- app/Http/Controllers/LivestreamController.php
+++ app/Http/Controllers/LivestreamController.php
@@ -13,9 +13,35 @@ use Illuminate\Support\Str;
 
 class LivestreamController extends Controller
 {
-  public function showLiveStreamList(){
-    $LiveStreamList = Livestream::all();
-    return $LiveStreamList;
+  public function showLiveStreamList(Request $request){
+    // $LiveStreamList = Livestream::all();
+    // return $LiveStreamList;
+
+     // $this->request = $this->reformatRequest(Request::capture()->all());
+     if ($request['page']) {
+      $current_page = $request['page'];
+      // dd($current_page);
+    } else {
+      $current_page = 1;
+    }
+    if ($request['Livestreamnumber']) {
+      $show_product = $request['Livestreamnumber'];
+      // dd($show_product);
+    } else {
+      $show_product = 7;
+    }
+    $skip_product_in_page = ($current_page - 1) * $show_product;
+    $Livestream = Livestream::orderBy('id', 'asc')->where('Title', 'like', '%' . $request['Title'] . '%')->skip($skip_product_in_page)->take($show_product)->get();
+    $LivestreamTotal = Livestream::where('name', 'like', '%' . $request['Title'] . '%')->get();
+    $LivestreamNum = count($LivestreamTotal);
+    $resultJson = array(
+      'currentPage' => $current_page,
+      'LivestreamNumber' => count($Livestream),
+      'LivestreamMaxNumber' => $show_product,
+      'totalPage' => ceil($LivestreamNum / $show_product),
+      'result' => $Livestream
+    );
+    return $this->createJsonResult($resultJson);
   }
   public function addLivestream(Request $request){
     $validator = Validator::make(
